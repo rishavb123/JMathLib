f344c7e88eeac64b82eee5736dc929b88a479ac5
Rishav Bhagat <rishavb123@gmail.com>
2020-10-22 22:49:56 -0400

    made tests output cleaner

--- src/tests/TestUtils.java
+++ src/tests/TestUtils.java
@@ -2,6 +2,8 @@ package tests;
 
 public class TestUtils {
 
+    public static boolean withinTest = false;
+
     /**
      * Checks the results of a test
      * @param tag the tag or name of the test
@@ -10,20 +12,41 @@ public class TestUtils {
      * @param <T> the type of the result
      */
     public static <T> void check(String tag, T value, T expected) {
-        String status = "FAILED";
+        if(!withinTest) {
+            System.out.println(
+                    pad("Label", 50) + "| "
+                    + pad("Value", 50) + "| "
+                    + pad("Expected", 58) + "| "
+                    + "Status");
+            System.out.print("---------------------------------------------------------------------------------------");
+            System.out.println("-------------------------------------------------------------------------------------");
+            withinTest = true;
+        }
+
+        String status = ConsoleColors.RED_BOLD_BRIGHT + "FAILED" + ConsoleColors.RESET;
         try {
             if(value == expected || value.equals(expected)) {
-                status = "PASSED";
+                status = ConsoleColors.GREEN_BOLD_BRIGHT + "PASSED" + ConsoleColors.RESET;
             }
         } catch(Exception e) {}
-        System.out.println(tag + ": " + value + " - " + status + " (" + expected + " expected)");
+        System.out.println(
+                pad( tag, 50) + "| "
+                + pad(value.toString(), 50) + "| "
+                + pad(expected + " expected", 58) + "| "
+                + status
+        );
     }
 
     /**
      * Ends the test section with a new line
      */
     public static void endTest() {
+        withinTest = false;
         System.out.println();
+    }
+
+    private static String pad(String string, int length) {
+        return String.format("%-"+length+ "s", string);
     }
 
 }
--- src/tests/math/linearalgebra/TestVector.java
+++ src/tests/math/linearalgebra/TestVector.java
@@ -1,18 +1,19 @@
 package tests.math.linearalgebra;
 
 import io.bhagat.math.linearalgebra.Vector;
+import tests.TestUtils;
 
 public class TestVector {
 
     public static void main(String[] args) {
         Vector a = new Vector(2, 3, 4);
         Vector b = new Vector(-2, -3, -4);
-        System.out.println(Vector.add(a, b));
-        System.out.println(Vector.subtract(a, b));
-        System.out.println(Vector.dot(a, b));
-        System.out.println(a.equals(b));
-        System.out.println(a.equals(b.scale(-1)));
-        System.out.println((a.magnitude() == b.magnitude()) + " " + a.magnitude());
+        Vector c = new Vector(0, 0, 0);
+        Vector d = new Vector(4, 6, 8);
+        TestUtils.check("Test vector addition", Vector.add(a, b), c);
+        TestUtils.check("Test vector subtraction", Vector.subtract(a, b), d);
+        TestUtils.check("Test dot product", Vector.dot(a, b), -29.0);
+        TestUtils.check("Test magnitude equality", a.magnitude(), b.magnitude());
     }
 
 }
