8264c6bde61598b162d12a72cb965f6bfcb67351
Rishav Bhagat <rishavb123@gmail.com>
2020-10-30 22:49:52 -0400

    changes in tests

--- src/tests/math/linearalgebra/TestMatrix.java
+++ src/tests/math/linearalgebra/TestMatrix.java
@@ -1,9 +1,13 @@
 package tests.math.linearalgebra;
 
+import io.bhagat.math.linearalgebra.Matrix;
+import io.bhagat.math.linearalgebra.Vector;
+
 public class TestMatrix {
 
     public static void main(String[] args) {
-
+        Matrix A = new Matrix(new Vector(2, 3, 4), new Vector(5, 6, 7));
+        System.out.println(A);
     }
 
 }
